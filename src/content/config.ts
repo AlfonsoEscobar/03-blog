e6798374c9532192af7b433daa8793c1a095a3b0
alfonso.escobar.torrico <alfonso.escobar.torrico@gmail.com>
2024-10-23 16:13:56 +0000

    Ultimando las imagenes de los post

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,11 +3,13 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type: 'content',
-    schema: z.object({
-        title: z.string(), 
-        date: z.string(),
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        date: z.date(),
         description: z.string(),
-        image: z.string(),
+        image: image().refine((img) => img.width > 1080, {
+            message: "¡La imagen de portada debe tener al menos 1080 píxeles de ancho!",
+          }),
 
         // Relacion
         author: z.string(),
